50506251ad275c50efbefb9f8631de079721130c
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-08-05 10:41:12 +0330

    create employee entity

---
+++ src/main/java/com/example/spring_boot_testing/model/Employee.java
@@ -0,0 +1,25 @@
+package com.example.spring_boot_testing.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder // Implementation of Builder Design Pattern
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String email;
+
+}
