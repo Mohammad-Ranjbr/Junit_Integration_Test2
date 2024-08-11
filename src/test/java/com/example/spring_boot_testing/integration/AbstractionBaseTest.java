0c4a989120c25284d1dc699e19f6c26f5563969f
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-08-11 12:37:01 +0330

    add some comment

--- src/test/java/com/example/spring_boot_testing/integration/AbstractionBaseTest.java
+++ src/test/java/com/example/spring_boot_testing/integration/AbstractionBaseTest.java
@@ -8,7 +8,20 @@ import org.testcontainers.containers.PostgreSQLContainer;
 // The singleton container is started only once when the base class is loaded.
 // In this case, we manually perform the life cycle of the container
 
+// Singleton Container Pattern
+
 public abstract class AbstractionBaseTest {
+
+    // Advantages of this approach:
+    // Reducing code duplication: By using a base class (Base Class) such as AbstractionBaseTest, you can avoid code duplication.
+    // All test classes that inherit from this base class automatically have access to the initialized PostgreSQL container and its configurations.
+    // Better integration of tests: Your tests always have access to the same and isolated environment.
+    // This helps avoid problems with different development and production environments,
+    // because your tests are always running with the same database version and the same settings.
+    // Better resource management: Because the PostgreSQL container is started only once for all tests,
+    // you use resources more efficiently. This can reduce test execution time, especially in large projects with a large number of integrated tests.
+    // High flexibility: If you need to make changes to the database configuration, or PostgreSQL version,
+    // you can apply these changes only to the base class, and these changes will be automatically applied to all tests that inherit from this class.
 
     static final PostgreSQLContainer POSTGRE_SQL_CONTAINER;
 
@@ -17,6 +30,7 @@ public abstract class AbstractionBaseTest {
                 .withDatabaseName("ems")
                 .withUsername("postgres")
                 .withPassword("123456");
+        // to launch
         POSTGRE_SQL_CONTAINER.start();
     }
 
@@ -38,6 +52,7 @@ public abstract class AbstractionBaseTest {
     // Instead of manually and statically defining these values in application.properties or application.yml,
     // this code allows Spring Boot to automatically set these values at runtime.
     // Instead of storing configuration values statically in configuration files (such as application.properties), you can set them based on runtime conditions.
+
     @DynamicPropertySource
     public static void dynamicPropertySource(DynamicPropertyRegistry registry){
         registry.add("spring.datasource.url", POSTGRE_SQL_CONTAINER::getJdbcUrl);
